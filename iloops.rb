09f30b45f9f000b89e19429e3eadb686a81ffa34
Michel Perez <michel.ingesoft@gmail.com>
2015-09-19 14:32:18 -0500

    uploading formula

---
+++ iloops.rb
@@ -0,0 +1,14 @@
+class Iloops < Formula
+  desc "InfiniteLoops command line toolbelt"
+  homepage "http://infiniteloops.co/"
+  url "https://s3.amazonaws.com/iloops/toolbelt/iloops-0-0-1.tar.gz"
+  sha256 "2e8d28715b3143103430594ed9273f3b780ff8e36a3f65df92a36fab6cecd6c4"
+  version "1.1.0"
+
+  depends_on :x11 # if your formula requires any X11/XQuartz components
+
+  def install
+    bin.install "iloops"
+  end
+
+end
