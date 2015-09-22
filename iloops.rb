a1aec0b56905e6606c1d18854197edee02b7ab10
Michel Perez <michel.ingesoft@gmail.com>
2015-09-21 22:54:32 -0500

    changed sha

--- iloops.rb
+++ iloops.rb
@@ -2,7 +2,7 @@ class Iloops < Formula
   desc "InfiniteLoops command line toolbelt"
   homepage "http://infiniteloops.co/"
   url "https://s3.amazonaws.com/iloops/toolbelt/iloops-1.1.1.tar.gz"
-  sha256 "2e8d28715b3143103430594ed9273f3b780ff8e36a3f65df92a36fab6cecd6c4"
+  sha256 "645a6ff0a197b542967f10d606048acd54cc719f39e32b5159c0d07faa2eb902"
 
   depends_on :x11 # if your formula requires any X11/XQuartz components
 
