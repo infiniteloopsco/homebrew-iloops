56e528d2cb974c76118b45d1e223815065f7c8ed
Michel Perez <michel.ingesoft@gmail.com>
2015-10-08 23:09:05 -0500

    version 1.2.0

--- iloops.rb
+++ iloops.rb
@@ -1,8 +1,8 @@
 class Iloops < Formula
   desc "InfiniteLoops command line toolbelt"
   homepage "http://infiniteloops.co/"
-  url "https://s3.amazonaws.com/iloops/toolbelt/iloops-1.1.5.tar.gz"
-  sha256 "78f4799f2ef7d4b6e619fb8641f408f6554666e8e1203d1ea7f07311c99cd593"
+  url "https://s3.amazonaws.com/iloops/toolbelt/iloops-1.2.0.tar.gz"
+  sha256 "cb721c65b2097625d813a0cf1ef35cb146a8183d2dfd44ff6626fdaad80d0817"
 
   depends_on :x11 # if your formula requires any X11/XQuartz components
 
